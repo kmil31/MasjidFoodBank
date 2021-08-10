4826bea7359da4dab3d2c2b83c9005963aa737ca
AIMAN RAHIM <maiman.ar@live.iium.edu.my>
2021-08-11 07:26:06 +0800

    Pseudocode for functions

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-import user.js
-class testApp{
-    
+import {Recipient} from '.modules/user.js'
 
+
+function registerRecipient(){
+    //query for recipient name and IC to the database
+    //database returns boolean
+    //if(recipient does not exist in database)
+    let recipient = new Recipient('2','2','2')
+    //code: push into databaseHandler
+    //else
+    //show recipient already exist
+
+}
+
+function showRecipient(){
+    //ask for details
+    //establish connection to database
+    //query database for details
+    //return query result
+}
+
+function showAllRecipients(){
+    //query database
+    //return all in JSON
+    //put first 50 in array
+    //display in List
+}
+function removeRecipients(){
+    //
+    //
+    //
+    //
+    //
 }
