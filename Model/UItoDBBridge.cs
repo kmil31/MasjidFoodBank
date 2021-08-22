5396c814713e750ea673b380e3dc836c1a62b5af
AIMAN RAHIM <maiman.ar@live.iium.edu.my>
2021-08-23 01:05:01 +0800

    add UItoDBBridge (Pseudocode only)

---
+++ Model/UItoDBBridge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace god_does_it.Model
+{
+    class UItoDBBridge
+    {
+        public Boolean CheckDatabase(String IC)
+        {
+            //implements an Async function that connects to online DB and returns value
+            return false;
+        }
+        public void RegisterToDatabase(String name, String IC, String Address)
+        {
+            //implement an Async Function that connects to online DB and registers user
+        }
+    }
+}
