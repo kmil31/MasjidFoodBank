f1283ec02b36983597897bb37ca0b88405cee934
kmil31 <44165822+kmil31@users.noreply.github.com>
2021-08-27 12:09:23 +0800

    Leggo

---
+++ Model/UserModelRealm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Realms;
+
+namespace god_does_it.Resources.Models
+{
+    public class User : RealmObject
+    {
+        [PrimaryKey]
+        public string IC { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public string Telephone { get; set; }
+
+        public IList<string> Dates{get;}
+
+
+}   
+
+   
+
+}
--- SearchForm.cs
+++ SearchForm.cs
@@ -2,47 +2,72 @@
 using System;
 using System.Data;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using god_does_it.Resources.Models;
 
 namespace god_does_it
 {
     public partial class SearchForm : Form
     {
+        public RealmOps realmops = new RealmOps();
+        private static string Hello_World = "Yes";
+        private static String htmltext = $@"
+            <style>
+                h1 {{ text-align:center; }}
+                body {{ background-color : #1abc9c; }}
+            </style>
+            <body>
+                <p><h1>{Hello_World}</h1>This is html rendered text</p>
+            </body>";
+        
+
         public SearchForm()
         {
             InitializeComponent();
+            htmlPanel1.Text = htmltext.Normalize();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
+      
         private void SearchForm_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox4_TextChanged(object sender, EventArgs e)
-        {
-
-        }
+    
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var db = FakeDatabase.GetRecipients();
-            var x = from recipient in db
-                    where recipient.Name.Contains(textBox2.Text)
-                    select recipient;
-            foreach (var z in x)
-            {
-                Console.WriteLine(z.Name + "is Found");
-            }
+           
+            Hello_World = textBox2.Text;
+            htmlPanel1.Text = GenerateNewHTML(textBox2.Text).Normalize() ;
+            textBox2.Text = null;
+         
+        }
+
+        private void htmlPanel1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private String GenerateNewHTML(string x)
+        {
+            
+
+            var htmltext2 = $@"
+            <style>
+                h1 {{ text-align:center; }}
+                body {{ background-color : #1abc9c; }}
+            </style>
+            <body>
+                <p><h1></h1></p></br>
+                <div>
+                    Address : </br>
+                    IC      : </br>
+                    Phone Number: </br>
+                </div>
+            </body>";
+
+            return htmltext2;
         }
     }
 }
